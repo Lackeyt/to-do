b37ff4964cf8d81e2ab71a737299140540a63dcb
Erich Richter <erich.richter@gmail.com>
2020-06-15 14:45:48 -0700

    create initial files > > co-authored-by: Tyson Lackey <lackeyt90@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,33 @@
-$(document).ready(function(){
-
-});
 
 //Business Logic for Todo List
 function Todo(){
-  listItem = []
-  itemID  = 0
+    listItem = []
+    itemID  = 0
 }
 
 Todo.prototype.addItem = function(item) {
-  item.itemID = this.assignId();
-  this.listItem.push(item)
+    item.itemID = this.assignId();
+    this.ListItem.push(item)
 }
 
 Todo.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
+    this.currentId += 1;
+    return this.currentId;
 }
 
 //Business Logic for Items
 function ListItem(item) {
-  this.item = item
+    this.item = item
 }
 
+
+
+$(document).ready(function(){
+    $("#inputForm").submit(function(){
+        event.preventDefault()
+
+        let listItem = $("#item").val()
+        $("#output").append("<p>" + listItem + "</p><br>")
+        //populate values
+    })
+});
